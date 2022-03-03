9d415c8b092bfddf043822e6eabd803122addfb6
Reeperto <tooblecain@gmail.com>
2022-03-02 16:05:23 -0800

    Rounded Corners

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import "./styles.scss"
 
 let winW: number;
 let winH: number;
+let pad: number = 20;
+
+let appDOM: HTMLElement = document.getElementById('app');
 
 // Creating the sketch itself
 const sketch = (p5: p5) => {
@@ -10,12 +13,12 @@ const sketch = (p5: p5) => {
 	// The sketch setup method 
 	p5.setup = () => {
         // Variable Declaration
-        winW = p5.windowWidth - 100;
-        winH = p5.windowHeight - 100;
+        winW = p5.windowWidth - pad;
+        winH = p5.windowHeight - pad;
 
 		// Creating and positioning the canvas
 		const canvas = p5.createCanvas(winW, winH);
-		canvas.parent("app");
+		canvas.parent(appDOM);
 
 		// Configuring the canvas
 		p5.background("black");
