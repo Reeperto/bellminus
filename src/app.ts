a892712215ee1ac2b733a95ae28e8e5dd9aee32f
Eli Griffiths <tooblecain@gmail.com>
2022-03-03 16:01:50 -0800

    Add Basic Time Features

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,43 @@
 import * as p5 from "p5"
+import { daysToWeeks, format, getDay, hoursToMilliseconds, parse, startOfDay, startOfToday, } from 'date-fns'
 import "./styles.scss"
 
-let winW: number;
-let winH: number;
-let pad: number = 20;
+/* 
+Retrieve proper schedule json data for the current date
+*/
+import rawScheduleData from "../src/schedule.json"
+const todaysScheduleData = () => {
+	let dayStart = startOfToday();
+	
+	switch (getDay(dayStart)) {
+		case 0: 
+		case 6:
+			return null;
+			break;
+		case 1:
+			return rawScheduleData.schedule.A;
+			break;
+		case 2:
+		case 4:
+			return rawScheduleData.schedule.B;
+			break;
+		case 3:
+		case 5:
+			return rawScheduleData.schedule.C;
+			break;
+	}
+}
+
+let test = parse("14:40", "k:mm", Date.now());
+
+console.log(test);
 
 let appDOM: HTMLElement = document.getElementById('app');
 
+// P5 Setup and Drawing
+let winW: number;
+let winH: number;
+let pad: number = 20;
 // Creating the sketch itself
 const sketch = (p5: p5) => {
 
@@ -21,17 +52,21 @@ const sketch = (p5: p5) => {
 		canvas.parent(appDOM);
 
 		// Configuring the canvas
-		p5.background("black");
+		p5.background("#00ff00");
 
 	};
     
     p5.windowResized = () => {
-        p5.resizeCanvas(winW, winH)
+		winW = p5.windowWidth - pad;
+        winH = p5.windowHeight - pad;
+
+		p5.resizeCanvas(winW, winH)
     };
 
 	// The sketch draw method
 	p5.draw = () => {
-
+		p5.background("#00ff00")
+		
 	};
 };
 
